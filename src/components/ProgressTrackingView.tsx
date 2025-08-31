5662f12498ee6ade90d08df88b19c8b6618d452f
ShinminHsu <shinminhsu@gmail.com>
2025-08-31 11:52:38 +0800

    fix: use currentChart.currentStitch to tracking auto-scrolling

--- src/components/ProgressTrackingView.tsx
+++ src/components/ProgressTrackingView.tsx
@@ -69,6 +69,7 @@ export default function ProgressTrackingView() {
   // Auto-scroll functionality
   useAutoScroll({
     currentProject,
+    currentChart,
     displayRound,
     isViewMode,
     patternContainerRef
